83569faf5a13779d2f3172a856845233cd7e2011
Stefano Chiodino <louderth@gmail.com>
2017-05-17 20:51:58 +0100

    Check for cooldown time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+extern crate time;
+
 use std::io::{self, Write};
+use time::{Tm,Duration};
 
 fn main() {
+    let mut last_launch: Option<Tm> = None;
+    let launch_cooldown = Duration::minutes(5);
     println!("Salutations, President. Please insert the password to ahniliate (if that's how you spell it) our enemies!");
 
     loop{
@@ -13,7 +18,22 @@ fn main() {
             }
         }
 
+        match last_launch {
+            Some(last_launch_time) => {
+                let since_last_launch = last_launch_time - time::now_utc();
+                if since_last_launch < launch_cooldown {
+                    println!(
+                        "Apologies, estimeed President, the minimum cooldown time of {:?} has not been respected, please wait {:?} more seconds",
+                        launch_cooldown,
+                        launch_cooldown - since_last_launch
+                    );
+                    continue;
+                }
+            },
+            None => {},
+        }
         
+        last_launch = Option::Some(time::now_utc());
     }    
 }
 
