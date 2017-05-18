284dc77d17cbfc122896bcaefefc205d201483d3
Lee Richardson <l.richardson@zoho.com>
2017-05-18 21:12:16 +0100

    Refactors http call of get_status.

--- src/api.rs
+++ src/api.rs
@@ -7,22 +7,30 @@ use std::io::Read;
 
 extern crate json;
 
-pub fn get_status() -> Result<bool, String> {
+fn http_get_data(address: &str) -> Result<String, String> {
     // Make call
     let http_client = Client::new();
-    let request_result =  http_client.get("http://gitland.azurewebsites.net:80/api/warheads/status")
+    let request_result =  http_client.get(address)
         .send();
 
-    let mut response = try!(request_result.map_err(|err| err.to_string()));
+    let mut http_response = try!(request_result.map_err(|err| err.to_string()));
 
     // Read response
-    let mut api_status_json = String::new();
-    let read_to_string_result = response.read_to_string(&mut api_status_json);
+    let mut response_data = String::new();
+    let read_to_string_result = http_response.read_to_string(&mut response_data);
 
     if !read_to_string_result.is_ok() {
         return Err(read_to_string_result.unwrap_err().to_string());
     }
 
+    return Ok(response_data);
+}
+
+pub fn get_status() -> Result<bool, String> {
+    let status_data_result = http_get_data("http://gitland.azurewebsites.net:80/api/warheads/status");
+
+    let api_status_json = try!(status_data_result);
+    
     // Parse response
     let parse_result = json::parse(api_status_json.as_str());
     let parsed = try!(parse_result.map_err(|err| err.to_string()));
