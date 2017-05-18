a04940a36274d92202397c0d76a023fc8ad8cb98
Lee Richardson <l.richardson@zoho.com>
2017-05-18 21:53:49 +0100

    Adds macro for slightly less repetative error handling.

--- src/api.rs
+++ src/api.rs
@@ -1,21 +1,23 @@
 extern crate hyper;
 use self::hyper::Client;
 
-use std;
 use std::result::Result;
 use std::io::Read;
 
 extern crate json;
 
+macro_rules! try_err_string {
+    ($x:expr) => (try!($x.map_err(|err| err.to_string())))
+}
+
 fn http_get_data(address: &str) -> Result<String, String> {
     // Make call
     let http_client = Client::new();
-    let request_result =  http_client.get(address)
-        .send();
+    let request_result = http_client.get(address).send();
 
-    let mut http_response = try!(request_result.map_err(|err| err.to_string()));
+    let mut http_response = try_err_string!(request_result);
 
-    // Read response
+    // Read response string
     let mut response_data = String::new();
     let read_to_string_result = http_response.read_to_string(&mut response_data);
 
@@ -30,14 +32,14 @@ pub fn get_status() -> Result<bool, String> {
     let status_data_result = http_get_data("http://gitland.azurewebsites.net:80/api/warheads/status");
 
     let api_status_json = try!(status_data_result);
-    
+
     // Parse response
     let parse_result = json::parse(api_status_json.as_str());
-    let parsed = try!(parse_result.map_err(|err| err.to_string()));
+    let parsed = try_err_string!(parse_result);
 
     // Determine status
     let api_status = parsed["Status"] == "Online";
-    
+
     return Result::Ok(api_status);
 }
 
