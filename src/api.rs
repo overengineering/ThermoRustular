136a8718b3316e5dc3de6f524e17e6588274572e
Lee Richardson <l.richardson@zoho.com>
2017-05-18 21:02:42 +0100

    Clean up get_status error handling.

--- src/api.rs
+++ src/api.rs
@@ -8,34 +8,29 @@ use std::io::Read;
 extern crate json;
 
 pub fn get_status() -> Result<bool, String> {
-    let client = Client::new();
-
-    let request_result = client
-        .get("http://gitland.azurewebsites.net:80/api/warheads/status")
+    // Make call
+    let http_client = Client::new();
+    let request_result =  http_client.get("http://gitland.azurewebsites.net:80/api/warheads/status")
         .send();
 
-    match request_result {
-        Ok(mut response) => {
-            let mut api_status_json = String::new();
-            let read_to_string_result = response.read_to_string(&mut api_status_json);
+    let mut response = try!(request_result.map_err(|err| err.to_string()));
 
-            match read_to_string_result {
-                Ok(_) => {
-                    let parse_result = json::parse(api_status_json.as_str());
-                    
-                    match parse_result {
-                        Ok(parsed) => {
-                            let api_status = parsed["Status"] == "Online";
-                            return Result::Ok(api_status);
-                        }
-                        Err(err) => return Err(err.to_string()),
-                    }
-                }
-                Err(err) => return Err(err.to_string()),
-            }
-        }
-        Err(err) => return Result::Err(err.to_string()),
+    // Read response
+    let mut api_status_json = String::new();
+    let read_to_string_result = response.read_to_string(&mut api_status_json);
+
+    if !read_to_string_result.is_ok() {
+        return Err(read_to_string_result.unwrap_err().to_string());
     }
+
+    // Parse response
+    let parse_result = json::parse(api_status_json.as_str());
+    let parsed = try!(parse_result.map_err(|err| err.to_string()));
+
+    // Determine status
+    let api_status = parsed["Status"] == "Online";
+    
+    return Result::Ok(api_status);
 }
 
 #[test]
